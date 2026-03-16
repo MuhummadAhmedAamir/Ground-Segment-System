15b8ec62161c1e19c4e3485bf14cb1f18deef913
Abdul Raafey <abdulraafey12345@gmail.com>
2026-03-16 09:42:55 +0500

    Conversion from Middleware to Routes

---
+++ backend/routes/windowRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { authenticate } = require('../middleware/auth');
+const { requireRole } = require('../middleware/rbac');
+const { communicationWindowOpen } = require('../controllers/windowController');
+const { dishAngle, BeginDishTransmission, EndDishTransmission } = require('../controllers/dishController');
+
+router.post('/', authenticate, requireRole('GROUND_STATION_OPERATOR'), communicationWindowOpen, dishAngle);
+router.post('/begin', authenticate, requireRole('GROUND_STATION_OPERATOR'), BeginDishTransmission);
+router.post('/end', authenticate, requireRole('GROUND_STATION_OPERATOR'), EndDishTransmission);
+
+module.exports = router; 
